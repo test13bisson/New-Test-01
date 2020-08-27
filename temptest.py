a7250d2c2139969b9641b5aee00611572bc903f0
mauricecevans <59456239+mauricecevans@users.noreply.github.com>
2020-08-27 12:40:27 -0600

    Update temptest.py

--- temptest.py
+++ temptest.py
@@ -10,3 +10,6 @@ my_third_oanda_access_credentials = "$pa33W0rd3!"
 
 oanda = opy.API(environment='practice',
                 access_token=my_oanda_access_credentials)
+
+
+awS_secret="7N1675LMTRM8PP9NF9E8M9C3F1EQ8PMP90P40P0K"
