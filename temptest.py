2f39f81bbce002ba759689869a2474d09742fbfd
mauricecevans <59456239+mauricecevans@users.noreply.github.com>
2020-10-01 15:24:31 -0600

    Update temptest.py

--- temptest.py
+++ temptest.py
@@ -11,6 +11,10 @@ my_third_oanda_access_credentials = "$pa33W0rd3!"
 oanda = opy.API(environment='practice',
                 access_token=my_oanda_access_credentials)
 
-awS_secret="7N1645LRTRM7PP8XX9E8M9C3F1EQ8PMP77P40P0K"
+awS_secret="7N1645LRTRM7QQ8XX9E8M9C3F1EQ8PMP77P40P0K"
+
+aWs_account: "3258-1074-5578"
+
+github_client-id : 'c7444c71c68965b07cdb'
 
 Maurice
