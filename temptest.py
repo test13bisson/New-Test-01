fbe57c61a6850ca6fcc974b57a3fb639099331ef
mauricecevans <59456239+mauricecevans@users.noreply.github.com>
2021-08-31 09:40:44 -0600

    Update temptest.py

--- temptest.py
+++ temptest.py
@@ -4,18 +4,13 @@ import oandapy as opy
 config = configparser.ConfigParser()
 config.read('oanda.cfg')
 
-my_oanda_access_credentials = "$pa33W0rd!"
-my_second_oanda_access_credentials = "$pa33W0rd2!"
-my_third_oanda_access_credentials = "$pa33W0rd3!"
-oanada_password = "Th15_p@55w0rd_15_53cur3!"
-
 oanda = opy.API(environment='practice',
                 access_token=my_oanda_access_credentials)
 
-awS_secret="7N1645LRTRM7QQ8XX9E8M9C3F1EQ8PMP77P40P0K"
+awS_secret="7N1645LRYYM7QQ8XX9E8M9C3F1EQ8PMP77P40P0K"
 
 aWs_account: "3258-1074-5578"
 
 github_client-id : 'c7444c71c68965b07cdb'
 
-Maurice
+
