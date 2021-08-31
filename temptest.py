d613f8661aad6826e0de36be39453ee6cdd3d0bd
mauricecevans <59456239+mauricecevans@users.noreply.github.com>
2021-08-31 10:39:27 -0600

    Update temptest.py

--- temptest.py
+++ temptest.py
@@ -14,7 +14,7 @@ oanda = opy.API(environment='practice',
 
 awS_secret="7N1645LRTRM7QQ8XX9E8M9C3F1EQ8PMP77P40P0K"
 
-aWs_account: "3258-1074-5578"
+aWs_account: "3002-1074-5578"
 
 github_client-id : 'c7444c71c68965b07cdb'
 
